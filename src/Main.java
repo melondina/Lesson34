abc4c5f03a2dc20cde7352165b343007afcf1067
Dina Migunova <migunova.dina@gmail.com>
2023-03-08 19:12:43 +0100

    Add printResult() method

--- src/Main.java
+++ src/Main.java
@@ -37,12 +37,7 @@ public class Main {
         Map<String, Integer> result = calculateResult(inputFile);
 
         File outputFile = new File("res/out.txt");
-        FileWriter outputFileWriter = new FileWriter(outputFile);
-
-        for (String name : result.keySet()) {
-            outputFileWriter.write(name + " " + result.get(name) + "\n");
-        }
-        outputFileWriter.close();
+        printResult(result, outputFile);
 
     }
     public static Map<String, Integer> calculateResult(File inputFile) throws IOException {
@@ -65,5 +60,14 @@ public class Main {
         }
         br.close();
         return result;
+    }
+    public static void printResult(Map<String, Integer> result, File outputFile) throws IOException {
+        FileWriter outputFileWriter = new FileWriter(outputFile);
+
+        for (String name : result.keySet()) {
+            outputFileWriter.write(name + " " + result.get(name) + "\n");
+        }
+        outputFileWriter.close();
+
     }
 }
